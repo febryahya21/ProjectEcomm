42b679de37222091270d413b0d58055360b69ed9
febryahya21 <febryahya21@gmail.com>
2021-04-14 17:03:57 +0700

    pertama commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# shopping-cart
+
+## database
+- Buat database baru dengan nama <b>shopping-cart</b>
+- Import file <b>shopping-cart.sql</b> yang ada dalam folder database
+
+## website:
+https://www.plajarikode.com/2020/05/membuat-shopping-cart-dengan-php.html
---
+++ database/shopping-cart.sql
@@ -0,0 +1,143 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 19, 2020 at 08:51 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `shopping-cart`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_detail_order`
+--
+
+CREATE TABLE `tb_detail_order` (
+  `id_detail` int(11) NOT NULL,
+  `id_order` int(11) NOT NULL,
+  `id_produk` int(11) NOT NULL,
+  `pembelian` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_detail_order`
+--
+
+INSERT INTO `tb_detail_order` (`id_detail`, `id_order`, `id_produk`, `pembelian`) VALUES
+(1, 44, 1, 2),
+(2, 44, 2, 2),
+(3, 45, 1, 1),
+(4, 45, 2, 3),
+(5, 46, 1, 2),
+(6, 46, 2, 3),
+(7, 46, 3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_order`
+--
+
+CREATE TABLE `tb_order` (
+  `id_order` int(10) UNSIGNED NOT NULL,
+  `total_item` int(11) NOT NULL,
+  `total_bayar` float NOT NULL,
+  `tgl_transaksi` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_order`
+--
+
+INSERT INTO `tb_order` (`id_order`, `total_item`, `total_bayar`, `tgl_transaksi`) VALUES
+(44, 4, 600000, '2020-05-17'),
+(45, 4, 700000, '2020-05-19'),
+(46, 6, 1100000, '2020-05-19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_produk`
+--
+
+CREATE TABLE `tb_produk` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nama_produk` varchar(50) NOT NULL,
+  `harga` float NOT NULL,
+  `stok` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_produk`
+--
+
+INSERT INTO `tb_produk` (`id`, `nama_produk`, `harga`, `stok`) VALUES
+(1, 'Sony1', 100000, 2),
+(2, 'Apple1', 200000, 3),
+(3, 'Samsung1', 300000, 4),
+(4, 'Apple2', 400000, 5);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_detail_order`
+--
+ALTER TABLE `tb_detail_order`
+  ADD PRIMARY KEY (`id_detail`);
+
+--
+-- Indexes for table `tb_order`
+--
+ALTER TABLE `tb_order`
+  ADD PRIMARY KEY (`id_order`);
+
+--
+-- Indexes for table `tb_produk`
+--
+ALTER TABLE `tb_produk`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_detail_order`
+--
+ALTER TABLE `tb_detail_order`
+  MODIFY `id_detail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tb_order`
+--
+ALTER TABLE `tb_order`
+  MODIFY `id_order` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+
+--
+-- AUTO_INCREMENT for table `tb_produk`
+--
+ALTER TABLE `tb_produk`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
